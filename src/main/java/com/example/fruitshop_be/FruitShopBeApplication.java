6dbc04354cba64b6e622219c979cccd807834512
H82M19\Student <Student>
2025-10-20 16:58:31 +0700

    init-project

---
+++ src/main/java/com/example/fruitshop_be/FruitShopBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.fruitshop_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitShopBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FruitShopBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fruitshop_be/FruitShopBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fruitshop_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitShopBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
