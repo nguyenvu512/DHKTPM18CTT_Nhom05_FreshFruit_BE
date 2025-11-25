bf485b39dc9b13bdea4e24fe62e36f7e0117a178
duy1606 <duyzzzzz405@gmail.com>
2025-11-25 13:56:41 +0700

    config-cross

--- src/main/java/com/example/fruitshop_be/configuration/SecurityConfig.java
+++ src/main/java/com/example/fruitshop_be/configuration/SecurityConfig.java
@@ -20,6 +20,8 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -64,5 +66,18 @@ public class SecurityConfig {
         JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
         return  jwtAuthenticationConverter;
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // áp dụng cho tất cả endpoint
+                        .allowedOrigins("http://localhost:5173") // domain frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true); // nếu dùng cookie/auth
+            }
+        };
     }
 }
